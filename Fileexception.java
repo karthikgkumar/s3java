2318b941a9d42fb1b8eb6bf1b2d5186668338209
karthikgkumar <karthikgkumar2002@gmail.com>
2022-10-26 16:16:01 +0530

    file exception is added

---
+++ Fileexception.java
@@ -0,0 +1,24 @@
+import java.io.FileInputStream;
+
+import java.io.*;
+public class Fileexception {
+   public static void main(String[] args) {
+       try{
+           FileInputStream fin = new FileInputStream("hello.txt");
+           FileOutputStream fout = new FileOutputStream("copy.txt");
+           int i;
+           while((i=fin.read()) != -1){
+               System.out.println((char)i);
+               fout.write(i);
+           }
+           fin.close();
+           fout.close();
+       }
+       catch(FileNotFoundException e){
+           System.out.println(e.getMessage());
+       }
+       catch(IOException e){
+           System.out.println(e.getMessage());
+       }
+   } 
+}
